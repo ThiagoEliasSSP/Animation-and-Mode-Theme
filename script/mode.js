e5f91d0b586399960753dc91c8e49fad6d4d4601
Thiago Elias <thiagoeliaspereira66@gmail.com>
2021-05-06 01:05:14 -0300

    Refazendo o código

--- script/mode.js
+++ script/mode.js
@@ -1,9 +1,7 @@
 const html = document.querySelector('html');
-const mode = document.getElementById('mode');
-const img = document.getElementById('background')
+const btn = document.getElementById('btn')
+const icon = document.querySelector('#icon')
 
-mode.addEventListener('click', function() {
-  html.classList.toggle('dark-mode')
-  html.classList == 'dark-mode' ? img.setAttribute('src', '/img/background_dark.png') : img.setAttribute('src', '/img/background_light.png')
-  html.classList == 'dark-mode' ? mode.innerHTML = 'LightMode' : mode.innerHTML = 'DarkMode'
+btn.addEventListener('click', function () {
+    html.classList.toggle('dark')
 })
